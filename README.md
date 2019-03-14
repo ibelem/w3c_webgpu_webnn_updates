cf4b645b45024108cdc15b65aa06c7f5af1bdeda
Belem Zhang <belem.zhang@intel.com>
2019-03-14 14:29:59 +0800

    update version

--- README.md
+++ README.md
@@ -3,4 +3,28 @@
 
 * [W3C WebGPU 技术进展及社区组状态更新 2018.11.17](https://github.com/ibelem/w3c_webgpu_webnn_updates/blob/master/WebGPU%20-%20W3C%20Chinese%20Web%20IG%202018.pdf)
 
-* [W3C WebML 技术进展及社区组状态更新 2018.11.17](https://github.com/ibelem/w3c_webgpu_webnn_updates/blob/master/WebML%20-%20W3C%20Chinese%20Web%20IG%202018.pdf)
+* [W3C WebML 技术进展及社区组状态更新 2018.11.17](https://github.com/ibelem/w3c_webgpu_webnn_updates/blob/master/WebML%20-%20W3C%20Chinese%20Web%20IG%202018.pdf) - Web Neural Network API / Web Machine Learning API
+
+
+## Scripts
+
+### `npm start`
+
+Runs the app in the development mode.<br>
+Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
+
+
+### `npm test`
+
+Launches the test runner in the interactive watch mode.<br>
+See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
+
+### `npm run build`
+
+Builds the app for production to the `build` folder.<br>
+It correctly bundles React in production mode and optimizes the build for the best performance.
+
+The build is minified and the filenames include the hashes.<br>
+Your app is ready to be deployed!
+
+See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
---
+++ build/precache-manifest.54b3254b6dbcdf1da3e55566617ee5b5.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = [
+  {
+    "revision": "6c970f5fa1be73bd4eff6e9c9d5d38e6",
+    "url": "/static/media/proposal.6c970f5f.png"
+  },
+  {
+    "revision": "ef4bfd58d7c8f2d09899",
+    "url": "/static/js/main.ef4bfd58.chunk.js"
+  },
+  {
+    "revision": "222dea9f3528ed08fb84a497256806eb",
+    "url": "/static/media/cooperation1.222dea9f.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "bb0c87053d716bfc7735e9b3a68a955d",
+    "url": "/static/media/webglstack.bb0c8705.png"
+  },
+  {
+    "revision": "2a2872f4e6cda4c47bb796334774f552",
+    "url": "/static/media/gpucgqr.2a2872f4.png"
+  },
+  {
+    "revision": "f43a69da9658463c4da986c516ff4bd3",
+    "url": "/static/media/glversion.f43a69da.png"
+  },
+  {
+    "revision": "7582e1ddee0a8a979010d6c66210eff7",
+    "url": "/static/media/webgltogpu.7582e1dd.png"
+  },
+  {
+    "revision": "8535a03cd8bdccd47294c552180c9aab",
+    "url": "/static/media/performancegap.8535a03c.png"
+  },
+  {
+    "revision": "8d14b0f517907050da1c26f8e32c27bd",
+    "url": "/static/media/performancegap2.8d14b0f5.png"
+  },
+  {
+    "revision": "5f9041838ea4ae25a1ae278dc52eac93",
+    "url": "/static/media/jsf.5f904183.png"
+  },
+  {
+    "revision": "3dec98a338ec1a3086e3786d4d99ce6f",
+    "url": "/static/media/demo2.3dec98a3.png"
+  },
+  {
+    "revision": "bc2424a42a53aae2655c25e4e0a9ca1d",
+    "url": "/static/media/apistack.bc2424a4.png"
+  },
+  {
+    "revision": "ed6b16be7c465642b8b23642ae1ebb7e",
+    "url": "/static/media/apipoc.ed6b16be.png"
+  },
+  {
+    "revision": "f9819658fdb1eaadc8bc5788f6a71088",
+    "url": "/static/media/perf1.f9819658.png"
+  },
+  {
+    "revision": "c7408098f8cf76914f1fa193eaeeebdf",
+    "url": "/static/media/perf2.c7408098.png"
+  },
+  {
+    "revision": "10df31c6aed86e1ff6c8ef297ea08383",
+    "url": "/static/media/cgqr.10df31c6.png"
+  },
+  {
+    "revision": "5d1da7a127dce637d38ac05174f3f4e1",
+    "url": "/static/media/nnspec.5d1da7a1.png"
+  },
+  {
+    "revision": "5167bd6b1145540d616134bc58118f20",
+    "url": "/static/media/pqr.5167bd6b.png"
+  },
+  {
+    "revision": "cbce6abcc60f56cbfe05e3466f77e2cf",
+    "url": "/static/media/demo1.cbce6abc.png"
+  },
+  {
+    "revision": "bd5506759b1e9a723a5c",
+    "url": "/static/js/1.bd550675.chunk.js"
+  },
+  {
+    "revision": "bd5506759b1e9a723a5c",
+    "url": "/static/css/1.ddc6d434.chunk.css"
+  },
+  {
+    "revision": "b0ebbe8cfaf986f0c85c5d88081ba3eb",
+    "url": "/index.html"
+  }
+];
